ba0d4e1baa79151c2f01138dc6195683900967fb
Seongh78 <seongh7800@gmail.com>
2019-06-30 15:35:03 +0900

    first setting

---
+++ includes/config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Simple PHP Website',
+        'site_url' => 'http://localhost/simple_api',
+        'pretty_uri' => true,
+        'nav_menu' => [
+            '' => 'Home',
+            'about-us' => 'About Us',
+            'products' => 'Products',
+            'contact' => 'Contact',
+        ],
+        'template_path' => 'template',
+        'content_path' => 'views',
+        'api_path' => 'controllers',
+        'version' => 'v3.0',
+    ];
+
+    return isset($config[$key]) ? $config[$key] : null;
+}
